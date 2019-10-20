36bb22d14d2f33561173b57a1430ed814ee7cbc3
Andrzej Kolomanski <jedrasek666@gmail.com>
2019-10-20 17:41:36 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import SEO from '../components/seo';
 
-export default IndexPage
+const IndexPage = ({ data }) => (
+	<Layout>
+		<SEO title="Home" />
+		<ul>
+			{data.allStrapiWine.edges.map((document) => (
+				<li>
+					<h2>
+						<Link to={`${document.node.id}`}>{document.node.Name}</Link>
+					</h2>
+          <img src={document.node.photos.publicURL} alt={`${document.node.id} wine`}/>
+          <p>{document.node.description}</p>
+				</li>
+			))}
+		</ul>
+	</Layout>
+);
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+	query {
+		allStrapiWine {
+			edges {
+				node {
+					Name
+					Price
+					description
+					id
+					photos {
+						publicURL
+					}
+				}
+			}
+		}
+	}
+`;
